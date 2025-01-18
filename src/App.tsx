d1ad3d07955e8fa1ddd16722e53844253e01d29f
Archit Jain <architjain798@gmail.com>
2025-01-18 13:46:50 +0530

    initialize React + TypeScript + Vite project with basic configuration and structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div>
+     <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, sed.</h1>
+    </div>
+  )
+}
+
+export default App
