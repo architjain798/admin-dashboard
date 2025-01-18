702c6fc27f583b0e6121e351ae60870ad4b2b751
Archit Jain <architjain798@gmail.com>
2025-01-18 14:04:26 +0530

    add routing with react-router-dom and create HomePage and Categories components

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+
+function HomePage() {
+
+  return (
+    <div>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, sed.</h1>
+    </div>
+  )
+}
+
+export default HomePage
