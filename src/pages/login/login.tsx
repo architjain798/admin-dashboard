41efb2d5beead330a13517df21fdb6d6c13f185c
Archit Jain <architjain798@gmail.com>
2025-01-18 16:38:01 +0530

    feat: add LoginPage component with basic structure and tests; configure testing environment

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,16 @@
+
+const LoginPage = () => {
+  return (
+    <>
+    <h1>Sign in</h1>
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <button>Login in</button>
+    <label htmlFor="remember-me">Remember me</label>
+    <input type="checkbox" name="" id="remember-me" />
+    <a href="#">Forgot password</a>
+    </>
+  )
+}
+
+export default LoginPage
